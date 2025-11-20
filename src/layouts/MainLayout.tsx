7a23396142c0c12aeb0eeef93812f34efb83205a
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-11-20 17:43:26 +0530

    add the main layout

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-white via-yellow-50 to-white">
+      {children}
+    </div>
+  );
+};
